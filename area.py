7bb3360bce633c729abb1815272dd79369e5705d
Mihajlo <99297361+MihajloVSc@users.noreply.github.com>
2024-04-22 13:43:38 +0200

    Update area.py

--- area.py
+++ area.py
@@ -1,6 +1,6 @@
-length = float(input("Length")) # Ask you for a info on length of square
-width = float(input("Width")) # Ask you for info on width of square
+length = float(input("Length >> ")) # Ask you for a info on length of square
+width = float(input("Width >> ")) # Ask you for info on width of square
 
 x = length * width #calculate
 
-print("The Area of a square is >>  " , x) #execute
+print("The Area of a square is: " , x) #execute
